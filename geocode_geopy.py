45190bfb8b3bab141a0aaa6350560401f5f4bc5e
itinstructor <32806514+itinstructor@users.noreply.github.com>
2021-08-15 15:04:34 -0600

    Update geocode_geopy.py

--- geocode_geopy.py
+++ geocode_geopy.py
@@ -8,6 +8,10 @@
 
 from geopy.geocoders import Nominatim
 
+# For testing
+# LAT =
+# LON =
+
 
 def main():
     # For testing
@@ -16,41 +20,47 @@ def main():
 
 
 def geocode():
-    # Create geolocator object with Nominatim geocode service
-    # Nominatim is a free geolocater that uses openstreetmaps.org
-    geolocator = Nominatim(user_agent="location_practice")
+    try:
+        # Create geolocator object with Nominatim geocode service
+        # Nominatim is a free geolocater that uses openstreetmaps.org
+        geolocator = Nominatim(user_agent="location_practice")
 
-    # Get location input from user
-    city = input("Enter city: ")
-    state = input("Enter state: ")
-    country = input("Enter country: ")
+        # Get location input from user
+        city = input("Enter city: ")
+        state = input("Enter state: ")
+        country = input("Enter country: ")
 
-    # Create location dictionary for request
-    location = {
-        "city": city,
-        "state": state,
-        "country": country
-    }
+        # Create location dictionary for request
+        location = {
+            "city": city,
+            "state": state,
+            "country": country
+        }
 
-    geo_location = geolocator.geocode(location)
-    # For testing purposes
-    # print(geo_location.raw)
-    # print(geo_location.address)
-    #print((geo_location.latitude, geo_location.longitude))
+        geo_location = geolocator.geocode(location)
+        # For testing purposes
+        # print(geo_location.raw)
+        # print(geo_location.address)
+        #print((geo_location.latitude, geo_location.longitude))
 
-    # Return geocode location information to calling program
-    return (geo_location.latitude, geo_location.longitude, geo_location.address)
+        # Return geocode location information to calling program
+        return (geo_location.latitude, geo_location.longitude, geo_location.address)
+    except:
+        print("An error occured while geocoding.")
 
 
 def reverse_geocode(lat, lon):
-    # Create geolocator object
-    geolocator = Nominatim(user_agent="location_practice")
-    # Create location tuple
-    location = (lat, lon)
-    # Get address with resolution of town
-    address = geolocator.reverse(location, zoom=10)
-    # print(address)
-    return address
+    try:
+        # Create geolocator object
+        geolocator = Nominatim(user_agent="location_practice")
+        # Create location tuple
+        location = (lat, lon)
+        # Get address with resolution of town
+        address = geolocator.reverse(location, zoom=10)
+        # print(address)
+        return address
+    except:
+        print("An error occured while reverse geocoding.")
 
 
 # If a standalone program, call the main function
